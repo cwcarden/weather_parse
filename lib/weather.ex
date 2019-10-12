7027ce1ddb5fedbe5d293eadd2ffc4d6a0cc8de2
charles <charles@charlescarden.com>
2019-10-12 09:27:01 -0500

    added forecast function

--- lib/weather.ex
+++ lib/weather.ex
@@ -1,7 +1,18 @@
 defmodule Weather do
-  def get_weather_map() do
-    api_key = "insert_api_key"
-    app_key = "insert app key"
-    Poison.decode!(HTTPoison.get!("https://api.ambientweather.net/v1/devices?applicationKey=#{app_key}&apiKey=#{api_key}").body)
+  def get_local do
+    api_key = ""
+    app_key = ""
+    data = Map.fetch(List.first(Poison.decode!(HTTPoison.get!("https://api.ambientweather.net/v1/devices?applicationKey=#{app_key}&apiKey=#{api_key}").body)), "lastData")
+    data = elem(data, 1)
   end
+
+def get_forecast do
+  longitude = "34.2091"
+  lattitude = "-105.8890"
+  api_key = ""
+  url = "https://api.darksky.net/forecast/#{api_key}/#{longitude},#{lattitude}?exclude=minutely,current,daily,flags"
+  data = Poison.decode!(HTTPoison.get!(url).body)
+
+  end
+
 end
