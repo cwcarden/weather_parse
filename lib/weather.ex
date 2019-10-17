735650d1e3fd25c05db15015c5eb5ff161f41c09
charles <charles@charlescarden.com>
2019-10-16 21:06:41 -0500

    added local_data tuples

--- lib/weather.ex
+++ lib/weather.ex
@@ -4,15 +4,19 @@ defmodule Weather do
     app_key = ""
     data = Map.fetch(List.first(Poison.decode!(HTTPoison.get!("https://api.ambientweather.net/v1/devices?applicationKey=#{app_key}&apiKey=#{api_key}").body)), "lastData")
     data = elem(data, 1)
+    last_rain = Map.fetch(data, "lastRain")
+    is_raining = Map.fetch(data, "eventrainin")
+    current_temp = Map.fetch(data, "tempf")
+    local_data = {last_rain, is_raining, current_temp}
   end
 
 def get_forecast do
-  longitude = "34.2091"
-  lattitude = "-105.8890"
+  longitude = "35.2091"
+  lattitude = "-101.8890"
   api_key = ""
   url = "https://api.darksky.net/forecast/#{api_key}/#{longitude},#{lattitude}?exclude=minutely,current,daily,flags"
   data = Poison.decode!(HTTPoison.get!(url).body)
-
+  Map.fetch(data, "currently")
   end
 
 end
